e364cf1939996219c88fd447b18cb522592bd5a3
Seethalakshmi Venkataramanan <seethalakshmi.venkataramanan@gmail.com>
2026-04-07 12:27:11 -0400

    Adeed echo.ts to make the echo instance accessed globally for Echo.join

---
+++ resources/js/lib/echo.ts
@@ -0,0 +1,16 @@
+import EchoClass from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+const Echo = new EchoClass({
+    broadcaster: "reverb",
+   // key: import.meta.env.VITE_REVERB_APP_KEY,
+  //  wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+  //  wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+  //  wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+ //   forceTLS: false,
+  //  enabledTransports: ["ws", "wss"],
+});
+
+export default Echo;
